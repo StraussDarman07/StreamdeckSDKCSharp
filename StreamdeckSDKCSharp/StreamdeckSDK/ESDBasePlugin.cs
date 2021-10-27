28bf5b1fe8ea6ebad41db7f100966591fadada2e
Thomas Stachl <Thomas.Stachl@corsair.com>
2021-10-27 22:42:25 +0200

    Added Subscriptions and virtual methods

--- StreamdeckSDKCSharp/StreamdeckSDK/ESDBasePlugin.cs
+++ StreamdeckSDKCSharp/StreamdeckSDK/ESDBasePlugin.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Elgato.StreamdeckSDK.Types.Messages;
+using Elgato.StreamdeckSDK.Types.Messages.ESDActions;
 
 namespace Elgato.StreamdeckSDK
 {
@@ -14,12 +16,52 @@ namespace Elgato.StreamdeckSDK
 
         private void SubscribeToStreamdeck()
         {
+            ESDConnectionManager.KeyDownForAction += OnKeyDown;
+            ESDConnectionManager.KeyUpForAction += OnKeyUp;
 
+            ESDConnectionManager.SendToPlugin += OnSendToPlugin;
+
+            ESDConnectionManager.DeviceDidDisconnect += OnDeviceDidDisconnect;
+            ESDConnectionManager.DeviceDidConnect += OnDeviceDidConnect;
+
+            ESDConnectionManager.ApplicationDidLaunch += OnApplicationDidLaunch;
+            ESDConnectionManager.ApplicationDidTerminate += OnApplicationDidTerminate;
+
+            ESDConnectionManager.DidReceiveSettings += OnReceiveSettings;
+            ESDConnectionManager.DidReceiveGlobalSettings += OnReceiveGlobalSettings;
+
+            ESDConnectionManager.PropertyInspectorAppeared += OnPropertyInspectorAppeared;
+            ESDConnectionManager.PropertyInspectorDisappeared += OnPropertyInspectorDisappeared;
+
+            ESDConnectionManager.TitleParametersChanged += OnTitleParametersChanged;
+
+            ESDConnectionManager.WillAppearForAction += OnWillAppearForAction;
+            ESDConnectionManager.WillDisappearForAction += OnWillDisappearForAction;
         }
 
         private void UnSubscribeFromStreamdeck()
         {
+            ESDConnectionManager.KeyDownForAction -= OnKeyDown;
+            ESDConnectionManager.KeyUpForAction -= OnKeyUp;
 
+            ESDConnectionManager.SendToPlugin -= OnSendToPlugin;
+
+            ESDConnectionManager.DeviceDidDisconnect -= OnDeviceDidDisconnect;
+            ESDConnectionManager.DeviceDidConnect -= OnDeviceDidConnect;
+
+            ESDConnectionManager.ApplicationDidLaunch -= OnApplicationDidLaunch;
+            ESDConnectionManager.ApplicationDidTerminate -= OnApplicationDidTerminate;
+
+            ESDConnectionManager.DidReceiveSettings -= OnReceiveSettings;
+            ESDConnectionManager.DidReceiveGlobalSettings -= OnReceiveGlobalSettings;
+
+            ESDConnectionManager.PropertyInspectorAppeared -= OnPropertyInspectorAppeared;
+            ESDConnectionManager.PropertyInspectorDisappeared -= OnPropertyInspectorDisappeared;
+
+            ESDConnectionManager.TitleParametersChanged -= OnTitleParametersChanged;
+
+            ESDConnectionManager.WillAppearForAction -= OnWillAppearForAction;
+            ESDConnectionManager.WillDisappearForAction -= OnWillDisappearForAction;
         }
 
         protected virtual void Dispose(bool disposing)
@@ -35,5 +77,21 @@ namespace Elgato.StreamdeckSDK
             Dispose(true);
             GC.SuppressFinalize(this);
         }
+
+        protected virtual void OnKeyDown(object sender, ESDKeyActionMessage keyAction) {}
+        protected virtual void OnKeyUp(object sender, ESDKeyActionMessage keyAction) {}
+        protected virtual void OnSendToPlugin(object sender, ESDSendToPluginActionMessage keyAction) {}
+        protected virtual void OnDeviceDidConnect(object sender, ESDDeviceConnectMessage keyAction) {}
+        protected virtual void OnDeviceDidDisconnect(object sender, ESDDeviceDisconnectMessage keyAction) {}
+        protected virtual void OnWillDisappearForAction(object sender, ESDAppearanceActionMessage e) {}
+        protected virtual void OnWillAppearForAction(object sender, ESDAppearanceActionMessage e){}
+        protected virtual void OnTitleParametersChanged(object sender, ESDTitleParametersChangeActionMessage e){}
+        protected virtual void OnPropertyInspectorDisappeared(object sender, ESDPropertyInspectorAppearanceActionMessage e){}
+        protected virtual void OnPropertyInspectorAppeared(object sender, ESDPropertyInspectorAppearanceActionMessage e){}
+        protected virtual void OnReceiveGlobalSettings(object sender, ESDGlobalSettingsMessage e){}
+        protected virtual void OnReceiveSettings(object sender, ESDSettingsActionMessage e){}
+        protected virtual void OnApplicationDidTerminate(object sender, ESDApplicationMessage e){}
+        protected virtual void OnApplicationDidLaunch(object sender, ESDApplicationMessage e){}
+
     }
 }
